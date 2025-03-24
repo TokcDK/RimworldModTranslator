f7b18364776e6bde39f9386390d5ff58e1b4d229
TokcDK <TokcDK@gmail.com>
2025-03-24 11:02:48 +0500

    set datacontext in pages behind

--- RimworldModTranslator/Views/ModListPage.xaml.cs
+++ RimworldModTranslator/Views/ModListPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimworldModTranslator.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace RimworldModTranslator.Views
         public ModListPage()
         {
             InitializeComponent();
+            DataContext = new ModListViewModel();
         }
     }
 }
