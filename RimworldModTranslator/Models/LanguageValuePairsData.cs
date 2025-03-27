7db71b3853c6605821919885deb1a5dbeaff22a9
TokcDK <TokcDK@gmail.com>
2025-03-27 15:34:47 +0500

    add languagevaluepairsdata

---
+++ RimworldModTranslator/Models/LanguageValuePairsData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RimworldModTranslator.Models
+{
+    public class LanguageValuePairsData()
+    {
+        public Dictionary<string, string> LanguageValuePairs { get; } = [];
+    }
+}
