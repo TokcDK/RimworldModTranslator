5e982f95bdb801fa7e2ece7bbd109ff0311bddda
TokcDK <TokcDK@gmail.com>
2025-03-28 20:48:15 +0500

    add ModAboutData

---
+++ RimworldModTranslator/Models/ModAboutData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimworldModTranslator.Models
+{
+    internal class ModAboutData
+    {
+        string? Name;
+        string? PackageId;
+        string? Authors;
+        string? ModVersion;
+        string? SupportedVersions;
+    }
+}
