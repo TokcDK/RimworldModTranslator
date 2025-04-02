0465cb1bb399f1f6e29b177f8c11a6faf9cbd231
TokcDK <TokcDK@gmail.com>
2025-04-02 17:12:30 +0500

    moved data to models

---
+++ RimworldModTranslator/Models/FolderData.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace RimworldModTranslator.Models
+{
+    public class FolderData
+    {
+        public string Name { get; set; }
+        public DataTable? TranslationsTable { get; set; }
+    }
+}
