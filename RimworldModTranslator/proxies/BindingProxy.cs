a11b52cf5d2efb362b2cd3f9ca48e3438ebd7ef9
TokcDK <TokcDK@gmail.com>
2025-04-23 13:14:17 +0500

    made translatable column headers using bindingproxy to not make the datagridextensions filtering broken like it was after previous solution

---
+++ RimworldModTranslator/proxies/BindingProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RimworldModTranslator.Proxies
+{
+    /// <summary>
+    /// Added in purpose to translate the datagrid column headers not breaking dotnet/datagridextensions filtering and maybe other things 
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        protected override Freezable CreateInstanceCore() => new BindingProxy();
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+    }
+}
